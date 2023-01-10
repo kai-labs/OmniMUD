a956f7d3b6b8d831f336ac2d57f97fa5fdfb6296
Michael Bledsoe <michael.bledsoe@gmail.com>
2023-01-10 16:32:00 -0500

    Adding google authentication

--- Solution/OmniMud.WebClient/Controllers/HomeController.cs
+++ Solution/OmniMud.WebClient/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace OmniMud.WebClient.Controllers
-{
+{    
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
-        }
+        }        
     }
 }
---
+++ Solution/OmniMud.WebClient/Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OmniMud.WebClient.Controllers
+{    
+    public class InfoController : Controller
+    {
+        public IActionResult GoogleHeaders()
+        {
+            var googleHeaders = new List<string>
+            {
+                "X-MS-TOKEN-GOOGLE-ID-TOKEN",
+                "X-MS-TOKEN-GOOGLE-ACCESS-TOKEN",
+                "X-MS-TOKEN-GOOGLE-EXPIRES-ON",
+                "X-MS-TOKEN-GOOGLE-REFRESH-TOKEN"
+            };
+
+            var output = Request.Headers
+                .Where(kvp => googleHeaders.Contains(kvp.Key))
+                .ToList();
+
+            return View(output);
+        }
+    }
+}
